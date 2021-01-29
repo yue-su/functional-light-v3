8246b3bd6424364453db0fb38e9ff2e942efcd6e
Yue Su <su2047@gmail.com>
2021-01-29 14:29:37 -0400

    Update ex.js

--- recursion/ex.js
+++ recursion/ex.js
@@ -1,15 +1,21 @@
-"use strict";
+"use strict"
 
-function isPalindrome() {}
+function isPalindrome(str) {
+  if (str.length <= 1) return true
+  if (str[0] === str[str.length - 1]) {
+    return isPalindrome(str.slice(1, str.length - 1))
+  }
+  return false
+}
 
-console.log( isPalindrome("") === true );
-console.log( isPalindrome("a") === true );
-console.log( isPalindrome("aa") === true );
-console.log( isPalindrome("aba") === true );
-console.log( isPalindrome("abba") === true );
-console.log( isPalindrome("abccba") === true );
+console.log(isPalindrome("") === true)
+console.log(isPalindrome("a") === true)
+console.log(isPalindrome("aa") === true)
+console.log(isPalindrome("aba") === true)
+console.log(isPalindrome("abba") === true)
+console.log(isPalindrome("abccba") === true)
 
-console.log( isPalindrome("ab") === false );
-console.log( isPalindrome("abc") === false );
-console.log( isPalindrome("abca") === false );
-console.log( isPalindrome("abcdba") === false );
+console.log(isPalindrome("ab") === false)
+console.log(isPalindrome("abc") === false)
+console.log(isPalindrome("abca") === false)
+console.log(isPalindrome("abcdba") === false)
